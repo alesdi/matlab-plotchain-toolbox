11b2022ed9280a3aec2f39500647862efa5bfedf
alessandrodiano <alessandro.diano@e-novia.it>
2021-04-30 10:46:56 +0200

    fix toolbox name typo

--- package.m
+++ package.m
@@ -1,8 +1,8 @@
 % Package the toolbox
 try
     dirPath = fileparts(which(mfilename));
-    projectPath = [dirPath '/mToolbox.prj'];
-    outputPath = [dirPath '/build/mToolbox'];
+    projectPath = [dirPath '/ptToolbox.prj'];
+    outputPath = [dirPath '/build/ptToolbox'];
     matlab.addons.toolbox.packageToolbox(projectPath, outputPath);
     
     fprintf('Toolbox successfully packaged:\n%s\n', outputPath);
